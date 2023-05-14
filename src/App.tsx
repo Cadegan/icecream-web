1ee75201640eb81a89c68f1ac8d43071b057c681
Cadegan <91974649+Cadegan@users.noreply.github.com>
2023-05-14 16:49:07 +0900

    spin-slow

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import sunLogo from "../src/data/logo/sun.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-orange-50 h-32">
+        <img src={sunLogo} alt="" className=" h-28 animate-spin-slow"></img>
       </header>
+      <section></section>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-5deg)" },
+          "50%": { transform: "rotate(5deg)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin 15s linear infinite",
+        wiggle: "wiggle 2s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
